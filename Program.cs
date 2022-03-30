dca8c3dc2de0d5f3800bb879ec74526cff5c0d38
Nguyễn Ngọc Bảo <bao201102@gmail.com>
2022-03-30 14:31:27 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace Do_an_OOP
             Console.WriteLine("Hello World!");
             
             ManageEmployee manageEmployee = new ManageEmployee();
+            Database db= new Database();
+            foreach (var item in db.Data)
+            {
+                manageEmployee.Add(item);
+            }
             manageEmployee.findbyExp(5);
             
         }
